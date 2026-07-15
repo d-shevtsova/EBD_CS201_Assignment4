f37decf736ab0d6fd2b800a9a7401b5eb702f65f
daria <daria.shevtsova.25@kse.org.ua>
2026-07-15 08:06:01 +0300

    зчитування 2 файлів

--- analytics.py
+++ analytics.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
+import csv
 
 # читаємо файли
+sales = []
 
+with open("global_sales.csv", "r") as sales_file:
+    reader = csv.DictReader(sales_file)
+    for row in reader:
+        sales.append(row)
+with open("regional_tariffs.json", "r") as file:
+    tariffs = json.load(file)
 # очистка від помилок
 
 # обчислення нет-профіту
