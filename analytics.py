6bb6b980a38d5c997e3a2aa29cb5a918e683d015
daria <daria.shevtsova.25@kse.org.ua>
2026-07-15 08:26:29 +0300

    Очищення файлів від помилок N/A

--- analytics.py
+++ analytics.py
@@ -13,7 +13,19 @@ with open("global_sales.csv", "r") as sales_file:
 with open("regional_tariffs.json", "r") as file:
     tariffs = json.load(file)
 # очистка від помилок
+for sale in sales:
+    if sale["quantity"] == "N/A":
+        sale["quantity"] = 0
+    else:
+        sale["quantity"] = int(sale["quantity"])
 
+    if sale["revenue"] == "N/A":
+        sale["revenue"] = 0.0
+    else:
+        sale["revenue"] = float(sale["revenue"])
+for region in tariffs.keys():
+    if tariffs[region] == "N/A":
+        tariffs[region] = 0
 # обчислення нет-профіту
 
 # новий датасет сlean_sales_upd
